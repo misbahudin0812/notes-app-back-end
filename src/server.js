74324c96fb29342e188ae021dcc71366c4caf07c
Muhammad Misbahudin <muhammad.misbahudin0812@gmail.com>
2024-10-07 14:54:58 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-  const server = Hapi.server({
+  constserver = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
